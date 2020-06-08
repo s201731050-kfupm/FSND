e28814b1ce55de35bbf8cdc2113593a97bca9cc3
s201731050-kfupm <s201731050@kfupm.edu.sa>
2020-06-08 13:40:18 +0300

    update

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
+    url: 'fsnd-try', // the auth0 domain prefix
+    audience: 'CoffeeShop', // the audience set for the auth0 app
+    clientId: 'ACJLUwTuW1Nrugs85mepG9t46cC7teYp', // the client id generated for the auth0 app
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
