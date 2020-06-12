148679de4a150533ece81c266521149b6307f28e
s201731050-kfupm <s201731050@kfupm.edu.sa>
2020-06-12 15:37:28 +0300

    Update app.py

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 def create_app(test_config=None):
-  # create and configure the app --
+  # create and configure the app --heroku config --app
   app = Flask(__name__)
   CORS(app)
 
