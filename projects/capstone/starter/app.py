f6ff0e4c4b5c7129fab50969901952cd204393e3
s201731050-kfupm <s201731050@kfupm.edu.sa>
2020-06-12 18:46:54 +0300

    testing

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 def create_app(test_config=None):
   # create and configure the app --heroku config --app
-  
+  # testing again
   app = Flask(__name__)
   CORS(app)
 
